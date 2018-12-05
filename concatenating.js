37a04f8f905957b1e5fe610bb26577bc2564137d
Paul Costa <paulcosta.student@careerdevs.com>
2018-12-05 01:30:52 +0000

    Concatentating

---
+++ concatenating.js
@@ -0,0 +1,37 @@
+// Concatenating Strings with Plus Operator
+// Concatenate means to link together. Think of the ’+’ operator like a chain linking strings together; add the strings just like you add numbers. Make sure your spelling is correct! Take note of spaces between words.
+
+var str = "Good " + "job!" // It says "Good job!"
+var abc = "Good" + "job!" // It says "Goodjob!"
+
+// Example
+var ourStr = "I come first. " + "I come second.";
+
+// Only change code below this line
+
+var myStr = "This is the start. " + "This is the end.";
+
+// Concatenating Strings with the Plus Equals Operator
+// The ’+=’ operator can concatenate (link) strings easily. Make sure your spelling is right, and you’ve left appropriate spaces.
+
+var str = "Hello ";
+str += "coding"; // Now the string reads "Hello coding"
+str += "camper!"; // And now the string reads "Hello codingcamper!"
+
+// Example
+var ourStr = "I come first. ";
+ourStr += "I come second.";
+
+// Only change code below this line
+
+var myStr = "This is the first sentence. ";
+myStr += "This is the second sentence.";
+
+// Example
+var ourName = "freeCodeCamp";
+var ourStr = "Hello, our name is " + ourName + ", how are you?";
+
+// Only change code below this line
+var myName = "Paul";
+var myStr = "My name is "+myName+" and I am well!";
+
